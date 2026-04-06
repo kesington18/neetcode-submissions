30135402ad57833e2671dc89749db79df05873be
Kesington Abdulwakeel <155372577+kesington18@users.noreply.github.com>
2026-04-06 17:36:32 +0100

    Add: remove-element - submission-6

---
+++ Data Structures & Algorithms/remove-element/submission-6.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        
+        for j in range(len(nums)):
+            if val != nums[j]:
+                nums[i] = nums[j]
+                i += 1
+
+        return i
